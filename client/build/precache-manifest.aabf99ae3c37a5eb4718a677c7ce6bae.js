c875f187be0e07b4c12ea391fa67c4febd63fc4e
Jiazi Cai <caijiazi777@gmail.com>
2020-05-06 12:46:49 -0400

    added

---
+++ client/build/precache-manifest.aabf99ae3c37a5eb4718a677c7ce6bae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe16cc697f3e489014313662d9ec8665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882eecc448c4dc241ce4",
+    "url": "/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "d75daf0af9ed6b53a69f",
+    "url": "/static/css/main.ea1027fc.chunk.css"
+  },
+  {
+    "revision": "882eecc448c4dc241ce4",
+    "url": "/static/js/2.d05defd6.chunk.js"
+  },
+  {
+    "revision": "d75daf0af9ed6b53a69f",
+    "url": "/static/js/main.2d4ec385.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "652d3a749b9304f74c8c32e44e6ad546",
+    "url": "/static/media/gallery.652d3a74.svg"
+  }
+]);
